6e9450f06fd6891f77af4de8bfff536c8cc2e57a
Joe Downard <joseph.downard@kainos.com>
2021-06-24 12:08:30 +0100

    Connected add employee page to the database

--- employee/app/routes.js
+++ employee/app/routes.js
@@ -33,24 +33,41 @@ router.get('/getEmployee', async (req, res) => {
 }
 )
 
+const addBaseEmployee = async (baseEmployee) => {
+    let results = await db.query('INSERT INTO Employee SET ?', baseEmployee) 
+    return results.insertId; 
+}
 
+const addSalesEmployee = async (salesEmployee) => {
+    let results = await db.query('INSERT INTO Sales_Employee SET ?', salesEmployee) 
+    return results.insertId; 
+}
+
+const addTechnicalEmployee = async (technicalEmployee) => {
+    let results = await db.query('INSERT INTO Technical_Employee SET ?', technicalEmployee) 
+    return results.insertId; 
+}
 
 router.get('/add-employee', (req, res) => {
     res.render('add-employee', {});
 });
 
-router.post('/add-employee', (req, res) => {
+router.post('/add-employee', async (req, res) => {
     var data = req.body;
-    baseEmployee = {name: data.name, address: data.address, nin: data.nin, ban: data.ban, sortcode: data.sortcode, salary: data.salary, department: data.department, manager: data.manager == '_unchecked' ? 0 : 1}
+    baseEmployee = {emp_name: data.name, address: data.address, nin: data.nin, ban: data.ban, sortcode: data.sortcode, salary: data.salary, department: data.department, manager: data.manager == '_unchecked' ? 0 : 1}
     console.log(baseEmployee)
+    id = await addBaseEmployee(baseEmployee)
+    console.log(id)
 
     if (data.salesOrTechnical == 'sales') {
         console.log("Adding sales employee")
-        salesEmployee = {commission_rate: data.commission_rate, total_sales_value: data.total_sales_value}
+        salesEmployee = {emp_id: id, commission_rate: data.commission_rate, total_sales_value: data.total_sales_value}
+        addSalesEmployee(salesEmployee)
         console.log(salesEmployee)
     } else if (data.salesOrTechnical == 'technical') {
         console.log("Adding technical employee")
-        technicalEmployee = {cv: data.cv, photo: data.photo}
+        technicalEmployee = {emp_id: id, cv: data.cv, photo: data.photo}
+        addTechnicalEmployee(technicalEmployee)
         console.log(technicalEmployee)
     } else {
         console.log("Adding non-technical/non-sales employee")

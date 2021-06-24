c571681e7ee28856d2321e5e4ec5beef804cbbb4
Joe Downard <joseph.downard@kainos.com>
2021-06-24 12:13:18 +0100

    Seperated db connection stuff to a different file.

--- employee/app/routes.js
+++ employee/app/routes.js
@@ -1,53 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-
-const mysql = require('mysql');
-const dbconfig = require('./dbconfig.json');
-const util = require('util')
-
-
-function wrapDB(dbconfig) {
-    const pool = mysql.createPool(dbconfig)
-    return {
-        query(sql, args) {
-            console.log("in query in wrapper")
-            return util.promisify(pool.query)
-                .call(pool, sql, args)
-        },
-        release() {
-            return util.promisify(pool.releaseConnection)
-                .call(pool)
-        }
-    }
-}
-
-const db = wrapDB(dbconfig)
-
-
-router.get('/getEmployee', async (req, res) => {
-    const result = await db.query(
-        "Select * from Employee"
-    )
-    console.log(result)
-}
-)
-
-const addBaseEmployee = async (baseEmployee) => {
-    let results = await db.query('INSERT INTO Employee SET ?', baseEmployee) 
-    return results.insertId; 
-}
-
-const addSalesEmployee = async (salesEmployee) => {
-    let results = await db.query('INSERT INTO Sales_Employee SET ?', salesEmployee) 
-    return results.insertId; 
-}
-
-const addTechnicalEmployee = async (technicalEmployee) => {
-    let results = await db.query('INSERT INTO Technical_Employee SET ?', technicalEmployee) 
-    return results.insertId; 
-}
-
 router.get('/add-employee', (req, res) => {
     res.render('add-employee', {});
 });
